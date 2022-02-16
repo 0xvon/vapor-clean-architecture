4704e16f3cce2f6d4a16b531a73a75dbff3a3c7d
masatsch <kingmasatojames@gmail.com>
2022-02-17 00:16:30 +0900

    first commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,22 @@
+import Fluent
+import FluentMySQLDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(.mysql(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .mysql)
+
+    app.migrations.add(CreateTodo())
+
+    // register routes
+    try routes(app)
+}
